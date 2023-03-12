a2512cb5087b562af605be551d9e83b60d6fa386
unknown <momen07996@gmail.com>
2023-03-12 08:38:51 -0700

    update the bootstrap borject

--- bootstrap_one/js1/indx.js
+++ bootstrap_one/js1/indx.js
@@ -1,251 +1,61 @@
-function click1() {
-    var image1 = document.getElementById("image1");
-    var image2 = document.getElementById("image2");
-    var image3 = document.getElementById("image3");
-    var image4 = document.getElementById("image4");
-    var image5 = document.getElementById("image5");
-    var image6 = document.getElementById("image6");
-    var image7 = document.getElementById("image7");
-    var image8 = document.getElementById("image8");
-
-    image1.style.opacity = "1";
-    image2.style.opacity = "1";
-    image3.style.opacity = "1";
-    image4.style.opacity = "1";
-    image5.style.opacity = "1";
-    image6.style.opacity = "1";
-    image7.style.opacity = "1";
-    image8.style.opacity = "1";
-
-    var button1 = document.getElementById("button1");
-    var button2 = document.getElementById("button2");
-    var button3 = document.getElementById("button3");
-    var button4 = document.getElementById("button4");
-    var button5 = document.getElementById("button5");
-    var button6 = document.getElementById("button6");
-
-    button1.style.backgroundColor = "red";
-    button1.style.color = "white";
-    button2.style.backgroundColor = "#eee";
-    button2.style.color = "black";
-    button3.style.backgroundColor = "#eee";
-    button3.style.color = "black";
-    button4.style.backgroundColor = "#eee";
-    button4.style.color = "black";
-    button5.style.backgroundColor = "#eee";
-    button5.style.color = "black";
-    button6.style.backgroundColor = "#eee";
-    button6.style.color = "black";
+var tag_header = document.querySelectorAll(".nav-link");
+for (let i = 0; i < tag_header.length; i++) {
+    var chekd = tag_header[i];
+    chekd.addEventListener("click", function () {
+        for (let i = 0; i < tag_header.length; i++) {
+            tag_header[i].classList.remove("actve");
+        }
+        this.classList.add("actve");
+    });
 }
 
-document.getElementById("button1").onclick = click1;
-
-function click2() {
-    var image1 = document.getElementById("image1");
-    var image2 = document.getElementById("image2");
-    var image3 = document.getElementById("image3");
-    var image4 = document.getElementById("image4");
-    var image5 = document.getElementById("image5");
-    var image6 = document.getElementById("image6");
-    var image7 = document.getElementById("image7");
-    var image8 = document.getElementById("image8");
-
-    image1.style.opacity = "1";
-    image2.style.opacity = "1";
-    image3.style.opacity = "0.5";
-    image4.style.opacity = "0.5";
-    image5.style.opacity = "0.5";
-    image6.style.opacity = "0.5";
-    image7.style.opacity = "0.5";
-    image8.style.opacity = "0.5";
-
-    var button1 = document.getElementById("button1");
-    var button2 = document.getElementById("button2");
-    var button3 = document.getElementById("button3");
-    var button4 = document.getElementById("button4");
-    var button5 = document.getElementById("button5");
-    var button6 = document.getElementById("button6");
-
-    button1.style.backgroundColor = "#eee";
-    button1.style.color = "black";
-    button2.style.backgroundColor = "red";
-    button2.style.color = "white";
-    button3.style.backgroundColor = "#eee";
-    button3.style.color = "black";
-    button4.style.backgroundColor = "#eee";
-    button4.style.color = "black";
-    button5.style.backgroundColor = "#eee";
-    button5.style.color = "black";
-    button6.style.backgroundColor = "#eee";
-    button6.style.color = "black";
+var button_image = document.querySelectorAll(".button");
+for (let i = 0; i < button_image.length; i++) {
+    var chekd = button_image[i];
+    chekd.addEventListener("click", function () {
+        for (let i = 0; i < button_image.length; i++) {
+            button_image[i].classList.remove("all");
+        }
+        this.classList.add("all");
+        var image = document.querySelectorAll(".div_images img");
+        image.forEach(element => {
+            element.style.opacity = "1";
+        });
+    });
 }
 
-document.getElementById("button2").onclick = click2;
+var click2 = document.getElementById("button2");
+click2.addEventListener("click", function () {
+    var image = document.querySelectorAll(".div_images img");
+    for (let i = 0; i <= 5; i++) {
+        image[i].style.opacity = "0.5";
+    }
+    image.style.opacity = "1";
+});
 
-function click3() {
-    var image1 = document.getElementById("image1");
-    var image2 = document.getElementById("image2");
-    var image3 = document.getElementById("image3");
-    var image4 = document.getElementById("image4");
-    var image5 = document.getElementById("image5");
-    var image6 = document.getElementById("image6");
-    var image7 = document.getElementById("image7");
-    var image8 = document.getElementById("image8");
+var click3 = document.getElementById("button3");
+click3.addEventListener("click", function () {
+    var image = document.querySelectorAll(".div_images img");
+    for (let i = 0; i <= 3; i++) {
+        image[i].style.opacity = "0.5";
+    }
+    image.style.opacity = "1";
+});
 
-    image1.style.opacity = "0.5";
-    image2.style.opacity = "0.5";
-    image3.style.opacity = "1";
-    image4.style.opacity = "1";
-    image5.style.opacity = "0.5";
-    image6.style.opacity = "0.5";
-    image7.style.opacity = "0.5";
-    image8.style.opacity = "0.5";
+var click4 = document.getElementById("button4");
+click4.addEventListener("click", function () {
+    var image = document.querySelectorAll(".div_images img");
+    for (let i = 0; i <= 1; i++) {
+        image[i].style.opacity = "0.5";
+    }
+    image.style.opacity = "1";
+});
 
-    var button1 = document.getElementById("button1");
-    var button2 = document.getElementById("button2");
-    var button3 = document.getElementById("button3");
-    var button4 = document.getElementById("button4");
-    var button5 = document.getElementById("button5");
-    var button6 = document.getElementById("button6");
-
-    button1.style.backgroundColor = "#eee";
-    button1.style.color = "black";
-    button2.style.backgroundColor = "#eee";
-    button2.style.color = "black";
-    button3.style.backgroundColor = "red";
-    button3.style.color = "white";
-    button4.style.backgroundColor = "#eee";
-    button4.style.color = "black";
-    button5.style.backgroundColor = "#eee";
-    button5.style.color = "black";
-    button6.style.backgroundColor = "#eee";
-    button6.style.color = "black";
-}
-
-document.getElementById("button3").onclick = click3;
-
-function click4() {
-    var image1 = document.getElementById("image1");
-    var image2 = document.getElementById("image2");
-    var image3 = document.getElementById("image3");
-    var image4 = document.getElementById("image4");
-    var image5 = document.getElementById("image5");
-    var image6 = document.getElementById("image6");
-    var image7 = document.getElementById("image7");
-    var image8 = document.getElementById("image8");
-
-    image1.style.opacity = "0.5";
-    image2.style.opacity = "0.5";
-    image3.style.opacity = "0.5";
-    image4.style.opacity = "0.4";
-    image5.style.opacity = "1";
-    image6.style.opacity = "1";
-    image7.style.opacity = "0.5";
-    image8.style.opacity = "0.5";
-
-    var button1 = document.getElementById("button1");
-    var button2 = document.getElementById("button2");
-    var button3 = document.getElementById("button3");
-    var button4 = document.getElementById("button4");
-    var button5 = document.getElementById("button5");
-    var button6 = document.getElementById("button6");
-
-    button1.style.backgroundColor = "#eee";
-    button1.style.color = "black";
-    button2.style.backgroundColor = "#eee";
-    button2.style.color = "black";
-    button3.style.backgroundColor = "#eee";
-    button3.style.color = "black";
-    button4.style.backgroundColor = "red";
-    button4.style.color = "white";
-    button5.style.backgroundColor = "#eee";
-    button5.style.color = "black";
-    button6.style.backgroundColor = "#eee";
-    button6.style.color = "black";
-}
-
-document.getElementById("button4").onclick = click4;
-
-function click5() {
-    var image1 = document.getElementById("image1");
-    var image2 = document.getElementById("image2");
-    var image3 = document.getElementById("image3");
-    var image4 = document.getElementById("image4");
-    var image5 = document.getElementById("image5");
-    var image6 = document.getElementById("image6");
-    var image7 = document.getElementById("image7");
-    var image8 = document.getElementById("image8");
-
-    image1.style.opacity = "0.5";
-    image2.style.opacity = "0.5";
-    image3.style.opacity = "0.5";
-    image4.style.opacity = "0.5";
-    image5.style.opacity = "0.5";
-    image6.style.opacity = "0.5";
-    image7.style.opacity = "1";
-    image8.style.opacity = "0.5";
-
-    var button1 = document.getElementById("button1");
-    var button2 = document.getElementById("button2");
-    var button3 = document.getElementById("button3");
-    var button4 = document.getElementById("button4");
-    var button5 = document.getElementById("button5");
-    var button6 = document.getElementById("button6");
-
-    button1.style.backgroundColor = "#eee";
-    button1.style.color = "black";
-    button2.style.backgroundColor = "#eee";
-    button2.style.color = "black";
-    button3.style.backgroundColor = "#eee";
-    button3.style.color = "black";
-    button4.style.backgroundColor = "#eee";
-    button4.style.color = "black";
-    button5.style.backgroundColor = "red";
-    button5.style.color = "white";
-    button6.style.backgroundColor = "#eee";
-    button6.style.color = "black";
-}
-
-document.getElementById("button5").onclick = click5;
-
-function click6() {
-    var image1 = document.getElementById("image1");
-    var image2 = document.getElementById("image2");
-    var image3 = document.getElementById("image3");
-    var image4 = document.getElementById("image4");
-    var image5 = document.getElementById("image5");
-    var image6 = document.getElementById("image6");
-    var image7 = document.getElementById("image7");
-    var image8 = document.getElementById("image8");
-
-    image1.style.opacity = "0.5";
-    image2.style.opacity = "0.5";
-    image3.style.opacity = "0.5";
-    image4.style.opacity = "0.5";
-    image5.style.opacity = "0.5";
-    image6.style.opacity = "0.5";
-    image7.style.opacity = "0.5";
-    image8.style.opacity = "1";
-
-    var button1 = document.getElementById("button1");
-    var button2 = document.getElementById("button2");
-    var button3 = document.getElementById("button3");
-    var button4 = document.getElementById("button4");
-    var button5 = document.getElementById("button5");
-    var button6 = document.getElementById("button6");
-
-    button1.style.backgroundColor = "#eee";
-    button1.style.color = "black";
-    button2.style.backgroundColor = "#eee";
-    button2.style.color = "black";
-    button3.style.backgroundColor = "#eee";
-    button3.style.color = "black";
-    button4.style.backgroundColor = "#eee";
-    button4.style.color = "black";
-    button5.style.backgroundColor = "#eee";
-    button5.style.color = "black";
-    button6.style.backgroundColor = "red";
-    button6.style.color = "white";
-}
-
-document.getElementById("button6").onclick = click6;
+var click5 = document.getElementById("button5");
+click5.addEventListener("click", function () {
+    var image = document.querySelectorAll(".div_images img");
+    for (let i = 0; i <= 0; i++) {
+        image[i].style.opacity = "0.5";
+    }
+    image.style.opacity = "1";
+});
